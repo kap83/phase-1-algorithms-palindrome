f239645edf7cf899e7f23a49e4a1692391880f83
Karen Polanco <polanco.k@gmail.com>
2022-12-06 18:17:42 -0500

    commit tree 6fbb2049e825b2a7ff287ff797dd6327504fe45e

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-function isPalindrome(word) {
+
   // Write your algorithm here
-}
+  
+  function isPalindrome (word) {
+    let c = word
+    .split("")
+    .reverse("")
+    .join("")
+    //console.log(word === j)
+    return word === c
+  }
+
+isPalindrome("")
 
 /* 
   Add your pseudocode here
+  takes an string and reverses the order of the letters 
+  compares the argument to the reversed version
+  returns T or F
+
+  Attempt 1: use split(), reverse(), join() to switch the argument, then console.logs a comparison. 
+
 */
 
 /*
   Add written explanation of your solution here
+  Create a function that tests for a palindrome
+  input: a word
+  output: true or false
+
+  function should accept 1 string as an argument: lower case, no spaces or special characters
 */
 
 // You can run `node index.js` to view these console logs
